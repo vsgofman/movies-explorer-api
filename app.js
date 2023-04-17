2bd82e130d6eff90f322f01547696af49bd12db0
Viktor Gofman <vsgof@yandex.ru>
2023-04-17 22:03:51 +0300

    feat: логирование запросов и ошибок

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const routes = require('./routes');
 const { auth } = require('./middlewares/auth');
 const disablePoweredBy = require('./middlewares/disablePoweredBy');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundApiError = require('./errors/NotFoundApiError');
 
 const app = express();
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/moviedb');
 app.use(disablePoweredBy);
 app.use(express.json());
 
+app.use(requestLogger);
 app.use(signin);
 app.use(signup);
 app.use(auth);
@@ -25,6 +27,7 @@ app.use('*', (req, res, next) => {
   next(new NotFoundApiError('Страница не найдена'));
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
